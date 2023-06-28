96aa248a2bb2290469615a1ceb3e67b130b63308
thientn <thientn@midota.com>
2023-06-28 17:55:16 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="">
+  <div>
+    <div>Tensorflow</div>
+    <input type="file" id="inputFile" accept="image/*" @change="handleFileChange"/>
+    <img src="" alt="" id="imgInvisible" ref="imgInvisible">
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import * as cocoSsd from '@tensorflow-models/coco-ssd';
+import '@tensorflow/tfjs-backend-cpu';
+import '@tensorflow/tfjs-backend-webgl';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  created() {
+    this.initModel();
+  },
+  methods: {
+    async initModel() {
+      try {
+        this.cocoModel = await cocoSsd.load();
+        console.log(this.cocoModel);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    handleFileChange(e) {
+      if (e.target.files < 1) return;
+      const reader = new FileReader();
+      const _this = this;
+      const image = document.getElementById('imgInvisible');
+      reader.onload = function () {
+        _this.$refs['imgInvisible'].src = reader.result;
+        const imageTag = new window.Image();
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+        imageTag.onload = function () {
+          image.setAttribute('width', `${imageTag.width}px`);
+          image.setAttribute('height', `${imageTag.height}px`);
+          _this.detectImage();
+        }
+        imageTag.src = reader.result;
+      }
+      reader.readAsDataURL(e.target.files[0]);
+    },
+    async detectImage() {
+      const image = document.getElementById('imgInvisible');
+      const result = await this.cocoModel.detect(image);
+      console.log(result);
+    }
+  },
+};
+</script>
